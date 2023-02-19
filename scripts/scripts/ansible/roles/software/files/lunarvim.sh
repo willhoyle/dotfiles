89b2581db9ac7dc2c58ab60926ab7b2472d44d81
Will Hoyle <will@Wills-MacBook-Pro.local>
2023-02-19 13:01:03 -0500

    ansible: cleanup for clean deploy

---
+++ scripts/scripts/ansible/roles/software/files/lunarvim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yes | LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
