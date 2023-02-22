7c610e9db50c0a6257f20a27bf3f22225fdcde53
Will Hoyle <will@Wills-MacBook-Pro.local>
2023-02-21 22:26:25 -0500

    ansible: fix things up, structure is mostly decided

--- scripts/scripts/ansible/roles/software/files/lunarvim.sh
+++ scripts/scripts/ansible/roles/software/files/lunarvim.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-yes | LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+# A hack to fix the lunarvim install script
+# it looks for tree-sitter but it's already installed by neovim 
+# js package and then fails
+python=$HOMEBREW_PREFIX/bin/python3
+
+# node deps
+npm i -g neovim
+
+# python deps
+eval "$python -m pip install pynvim"
+
+# rust deps
+cargo install "fd::fd-find"
+cargo install "rg::ripgrep"
+
+yes | LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh | sed '/tree-sitter-cli/d') "" --no-install-dependencies
